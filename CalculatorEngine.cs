6a933c7cf185c30e5f9df7861ec58a44e83dd216
semyanovame <semyanovame@309D-13>
2026-02-24 14:33:33 +0700

    нужно понять что вынести в классы ну и вынести соответственно. добавить xaml красоты, мб смену цвета кнопк

---
+++ CalculatorEngine.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculator
+{
+    public class CalculatorEngine
+    {
+        public double firstNumber {  get; set; }
+        public char operation {  get; set; }
+
+        public bool StartNewNumber = true;
+    }
+
+
+}
